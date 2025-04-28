3e85df7a23bb3a8bd78ec62fe83d457325bed445
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-04-28 21:37:28 +0500

    feat(hash): calcHash.js implemented

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,24 @@
+import { createHash } from "crypto";
+import { createReadStream } from "fs";
+import path from "path";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const filePath = path.join(
+    "src",
+    "hash",
+    "files",
+    "fileToCalculateHashFor.txt"
+  );
+  const fileStream = createReadStream(filePath);
+  const hash = createHash("sha256");
+
+  fileStream.pipe(hash);
+
+  hash.on("finish", (err) => console.log(hash.digest("hex")));
+
+  fileStream.on("error", (err) => {
+    console.error("Error reading the file:", err.message);
+  });
 };
 
 await calculateHash();
