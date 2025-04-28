156d0e205602e360eb49d2032e0d7c38ff7bb469
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-04-28 22:42:08 +0500

    feat(cp): cp.js implemented

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,29 @@
+import { spawn } from "child_process";
+import path from "path";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const scriptPath = path.resolve("src", "cp", "files", "script.js");
+
+  const child = spawn("node", [scriptPath, ...args]);
+
+  process.stdin.pipe(child.stdin);
+  
+  child.stdout.pipe(process.stdout);
+
+  child.stderr.on("data", (data) => console.error(`Child stderr: ${data}`));
+
+  child.on("close", (code) => {
+    code === 0
+      ? console.log("Child process finished successfully.")
+      : console.log(`Child process exited with code ${code}`);
+  });
+
+  await new Promise((resolve, reject) => {
+    child.on("close", (code) => {
+      if (code === 0) resolve();
+      else reject(`Child process failed with exit code: ${code}`);
+    });
+  });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["arg1", "arg2", "arg3"]);
