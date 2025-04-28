5a643cb034e1c83890f17654f58ed5b63d1ef400
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-04-28 17:38:15 +0500

    feat(fs): create.js implemented

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+import { promises as fs } from "fs";
+import path from "path";
+
 const create = async () => {
-    // Write your code here 
+  const filePath = path.join("src", "fs", "files", "fresh.txt");
+  try {
+    await fs.access(filePath);
+    throw new Error("FS operation failed");
+  } catch {
+    try {
+      await fs.writeFile(filePath, "I am fresh and young");
+    } catch {
+      throw new Error("FS operation failed");
+    }
+  }
 };
 
 await create();
