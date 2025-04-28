b33bae770f90569ab87b2073f6222bbd448dcead
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-04-28 18:05:21 +0500

    feat(fs): rename.js implemented

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,30 @@
+import { promises as fs } from "fs";
+import path from "path";
+
 const rename = async () => {
-    // Write your code here 
+  const oldPath = path.join("src", "fs", "files", "wrongFilename.txt");
+  const newPath = path.join("src", "fs", "files", "properFilename.txt");
+
+  try {
+    await fs.access(oldPath);
+  } catch {
+    throw new Error("FS operation failed");
+  }
+
+  try {
+    await fs.access(newPath);
+    throw new Error("FS operation failed");
+  } catch (error) {
+    if (error.code !== "ENOENT") {
+      throw new Error("FS operation failed");
+    }
+  }
+
+  try {
+    await fs.rename(oldPath, newPath);
+  } catch {
+    throw new Error("FS operation failed");
+  }
 };
 
 await rename();
