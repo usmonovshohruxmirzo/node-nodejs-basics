55b524fb1250a03b755fdc03c833dc3e68808970
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-04-28 19:43:32 +0500

    feat(fs): delete.js implemented

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,14 @@
+import { promises as fs } from "fs";
+import path from "path";
+
 const remove = async () => {
-    // Write your code here 
+  const filePath = path.join("src", "fs", "files", "fileToRemove.txt");
+  try {
+    await fs.access(filePath);
+    await fs.unlink(filePath);
+  } catch {
+    throw new Error("FS operation failed");
+  }
 };
 
 await remove();
