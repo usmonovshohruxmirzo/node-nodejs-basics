a92fd8b05005613d27c43447d4867120b82a5631
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-04-28 22:33:24 +0500

    feat(wt): main.js & worker.js implemented

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,31 @@
+import { Worker } from "worker_threads";
+import os from "os";
+import path from "path";
+
 const performCalculations = async () => {
-    // Write your code here
+  const numCores = os.cpus().length;
+  const workers = [];
+
+  const workerFilePath = path.resolve("src", "wt", "worker.js");
+
+  for (let i = 0; i < numCores; i++) {
+    workers.push(
+      new Promise((resolve) => {
+        const worker = new Worker(workerFilePath);
+
+        worker.postMessage(10 + i);
+
+        worker.on("message", (data) => resolve({ status: "resolved", data }));
+        worker.on("error", () => resolve({ status: "error", data: null }));
+        worker.on("exit", (code) => {
+          if (code !== 0) resolve({ status: "error", data: null });
+        });
+      })
+    );
+  }
+
+  const settledResults = await Promise.all(workers);
+  console.log(settledResults);
 };
 
 await performCalculations();
