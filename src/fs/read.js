3db45db3a0b2c141ddb6eaa8540a68b42d827324
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-04-28 20:22:41 +0500

    feat(fs): read.js implemented

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,16 @@
+import { promises as fs } from "fs";
+import path from "path";
+
 const read = async () => {
-    // Write your code here 
+  const filePath = path.join("src", "fs", "files", "fileToRead.txt");
+
+  try {
+    await fs.access(filePath);
+    const content = await fs.readFile(filePath, "utf-8");
+    console.log(content);
+  } catch {
+    throw new Error("FS operation failed");
+  }
 };
 
 await read();
