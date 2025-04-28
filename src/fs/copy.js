5cb877d862172688c6cf2ec68aece396b7433fc1
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-04-28 17:38:46 +0500

    feat(fs): copy.js implemented

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,24 @@
+import { promises as fs } from "fs";
+import path from "path";
+
 const copy = async () => {
-    // Write your code here 
+  const srcFolder = path.join("src", "fs", "files");
+  const destFolder = path.join("src", "fs", "files_copy");
+
+  try {
+    await fs.access(srcFolder);
+    await fs.access(destFolder);
+    throw new Error("FS operation failed");
+  } catch (err) {
+    if (err.message == "FS operation failed") {
+      throw err;
+    }
+    try {
+      await fs.cp(srcFolder, destFolder, { recursive: true });
+    } catch (err) {
+      throw new Error("FS operation failed");
+    }
+  }
 };
 
 await copy();
